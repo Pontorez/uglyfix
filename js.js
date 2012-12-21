00f55da06eeeed759561bd17a40113a9cf087fd8
Pontorez <pontorez@pontorez.com>
2012-12-21 11:26:19 +0000

    Another appengine proxy for kavkazcenter

--- js.js
+++ js.js
@@ -16,7 +16,9 @@ function x() {
         if (link.indexOf("http://www.kavkazcenter.com/") === 0) {
             if ($(this).parent().has('a.my').length === 0) {
                 var onionLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://2r2tz6wzqh7gaji7.onion.to/');
-                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION]</a>');
+                var appEngineLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://labnol-proxy-server.appspot.com/kavkazcenter.com/');
+                var iLikeProxyLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://ilikeproxy.appspot.com/kavkazcenter.com/');
+                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION</a> | <a target="_blank" href="' + appEngineLink + '">AE]</a> | <a target="_blank" href="' + iLikeProxyLink + '">ILP]</a>');
             }
         } else if (link.indexOf("http://www.newsru.com/") === 0) {
             if ($(this).parent().has('a.my').length === 0) {
