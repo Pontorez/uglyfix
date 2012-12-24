437bff1125b88a7d03386f1f4f408389c28186d8
Pontorez <pontorez@pontorez.com>
2012-12-24 16:26:12 +0000

    Stuff

--- js.js
+++ js.js
@@ -18,7 +18,7 @@ function x() {
                 var onionLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://2r2tz6wzqh7gaji7.onion.to/');
                 var appEngineLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://labnol-proxy-server.appspot.com/kavkazcenter.com/');
                 var iLikeProxyLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'http://ilikeproxy.appspot.com/kavkazcenter.com/');
-                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION</a> | <a target="_blank" href="' + appEngineLink + '">AE]</a> | <a target="_blank" href="' + iLikeProxyLink + '">ILP]</a>');
+                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION</a> <span class="red">|</span> <a target="_blank" href="' + appEngineLink + '">AE</a> <span class="red">|</span> <a target="_blank" href="' + iLikeProxyLink + '">ILP]</a>');
             }
         } else if (link.indexOf("http://www.newsru.com/") === 0) {
             if ($(this).parent().has('a.my').length === 0) {
