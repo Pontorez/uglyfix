6ce3cc875df29c97c7197a413020b4908514f6bb
Pontorez <pontorez@pontorez.com>
2013-02-25 11:54:36 +0000

    lj.rossia.org

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
+"use strict";
+
 $.fn.onAvailable = function (fn) {
     var sel = this.selector;
     var timer;
-//    var self = this;
     if (this.length > 0) {
         fn.call(this);
     }
@@ -22,20 +23,22 @@ if (String(location).indexOf('onion.to/enter.php?') != -1 || String(location).in
 }
 
 function x() {
-
     if (document.getElementById('ljtime')) {
         document.getElementById('ljtime').style.display = 'none';
     }
 
-    jQuery('a.entry-title-link').each(function () {
+    var appEngineLink, iLikeProxyLink;
+    var separator = ' <span class="red">|</span> ';
+
+    $('a.entry-title-link').each(function () {
 
         var link = $(this).attr('href');
         if (link.indexOf("http://www.kavkazcenter.com/") === 0) {
             if ($(this).parent().has('a.my').length === 0) {
                 var onionLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'https://2r2tz6wzqh7gaji7.onion.to/');
-                var appEngineLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'https://labnol-proxy-server.appspot.com/kavkazcenter.com/');
-                var iLikeProxyLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'https://ilikeproxy.appspot.com/kavkazcenter.com/');
-                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION</a> <span class="red">|</span> <a target="_blank" href="' + appEngineLink + '">AE</a> <span class="red">|</span> <a target="_blank" href="' + iLikeProxyLink + '">ILP]</a>');
+                appEngineLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'https://labnol-proxy-server.appspot.com/kavkazcenter.com/');
+                iLikeProxyLink = $(this).attr('href').replace('http://www.kavkazcenter.com/', 'https://ilikeproxy.appspot.com/kavkazcenter.com/');
+                $(this).parent().append(' <a target="_blank" href="' + onionLink + '" class="my">[ONION</a>' + separator + '<a target="_blank" href="' + appEngineLink + '">AE</a>' + separator + '<a target="_blank" href="' + iLikeProxyLink + '">ILP]</a>');
             }
         } else if (link.indexOf("http://www.newsru.com/") === 0) {
             if ($(this).parent().has('a.my').length === 0) {
@@ -43,10 +46,19 @@ function x() {
                 $(this).attr('href', palmLink);
                 $(this).parent().append(' <a target="_blank" href="' + palmLink + '" class="my">[palm version]</a>');
             }
+        } else if (link.indexOf("http://habrahabr.ru/") === 0) {
+            var entryAuthorName = $('.entry-author-name');
+            entryAuthorName.html(entryAuthorName.html().replace('alizar', '<span style="background-color: pink">alizar</span>'));
+        } else if (link.indexOf('http://lj.rossia.org/users/tiphareth/') === 0) {
+            if ($(this).parent().has('a.my').length === 0) {
+                appEngineLink = $(this).attr('href').replace('http://lj.rossia.org/users/tiphareth/', 'https://labnol-proxy-server.appspot.com/lj.rossia.org/users/tiphareth/');
+                iLikeProxyLink = $(this).attr('href').replace('http://lj.rossia.org/users/tiphareth/', 'https://ilikeproxy.appspot.com/lj.rossia.org/users/tiphareth/');
+                $(this).parent().append(' [<a class="my" target="_blank" href="' + appEngineLink + '">AE</a>' + separator + '<a target="_blank" href="' + iLikeProxyLink + '">ILP</a>]');
+            }
         }
     });
 }
-jQuery('body').keyup(function () {
+$('body').keyup(function () {
     x();
 }).mouseup(function () {
         setTimeout('x()', 66);
@@ -56,4 +68,3 @@ jQuery('body').keyup(function () {
         setTimeout('x()', 999);
     }
 );
-
