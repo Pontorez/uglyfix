81eb2242ea6078c690208d7438481675f32c29c3
Pontorez <pontorez@pontorez.com>
2013-07-03 14:50:04 +0000

    Newsru.com: get rid of unnecessary footer

--- js.js
+++ js.js
@@ -23,6 +23,9 @@ if (String(location).indexOf('onion.to/enter.php?') != -1 || String(location).in
 } else if (location.host == 'www.kcblog.info') {
     $('div.MsoNoSpacing').css('color', '#000');
 } else if (location.host == 'www.newsru.com') {
+    // В Chromium 30.0.1553.0 (209374) футер стал отображаться поверх текста. Скрываем его:
+    $('div.down').parent().parent().parent().parent().hide();
+    // Get rid of annoying background:
     document.body.style.background = '#555';
 }
 
