a2037c0ab7b408d20dc8f386e467f69e220f51fa
Pontorez <pontorez@pontorez.com>
2014-03-09 17:31:03 +0000

    Wikipedia: различия между версиями — переход на предыдущую/следующую правку с помощью C+Left / C+Right

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -6,4 +6,26 @@ $("a:contains('(G)')").each(function () {
     this.href = this.href.replace('hl=ru', 'hl=en');
 });
 
+// "Красивые" имена файлов при сохранении страниц
 document.title = document.title.replace(' — Википедия', '').replace(' - Wikipedia, the free encyclopedia', '');
+
+// Различия между версиями — переход на предыдущую/следующую правку с помощью C+Left / C+Right
+if (window.location.toString().indexOf('oldid')) {
+    window.onkeydown = function (e) {
+
+        e = e || window.event;
+        var keyCode = e.keyCode || e.which;
+
+        if (e.ctrlKey) {
+            try {
+                if (keyCode == 37) {
+                    document.getElementById('differences-prevlink').click();
+                } else if (keyCode == 39) {
+                    document.getElementById('differences-nextlink').click();
+                }
+            } catch (e) {
+                alert('Looks like you have reached the first or the most recent edit');
+            }
+        }
+    }
+}
