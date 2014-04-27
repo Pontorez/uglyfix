9ec0806c32a105e571d2c155166e245cbbcb1bc4
Pontorez <pontorez@pontorez.com>
2014-04-27 10:48:57 +0000

    intimcity.nl: C+Left/C+Right — переход на след./пред. страницу

--- js/intimcity.nl.js
+++ js/intimcity.nl.js
@@ -1,10 +1,23 @@
 "use strict";
 
+var isNoService = $('a.noservice:contains("Эротический")');
+if (isNoService.length) {
+    isNoService.css('background-color', 'red');
+    $('body').css('background-color', '#741717');
+}
+
 window.onkeydown = function (e) {
     e = e || window.event;
     var keyCode = e.keyCode || e.which;
-    if (keyCode == 27) {
+    var linkToClick;
+
+    if (keyCode == 27) { // Esc
         var bc = document.getElementsByClassName('ymaps-b-balloon__close');
         bc.length && bc[0].click();
+    } else if (keyCode == 37 && e.ctrlKey) {
+        linkToClick = $('a:contains("НАЗАД")');
+    } else if (keyCode == 39 && e.ctrlKey) {
+        linkToClick = $('a:contains("ДАЛЬШЕ")');
     }
+    linkToClick && linkToClick.length && linkToClick[0].click();
 };
