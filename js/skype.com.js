d00e297721e372a2b17feb695f60decb06e529cf
Pontorez <pontorez@pontorez.com>
2015-07-29 11:37:20 +0000

    Skype for Web: Auto Login

---
+++ js/skype.com.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+    $('#username').val(localStorage.getItem('username'));
+    $('#password').val(localStorage.getItem('password'));
+    setTimeout(function () {
+        $('#loginForm').submit();
+    }, 5000);
+});
+
+/*
+localStorage.setItem('username', 'xxx');
+localStorage.setItem('password', 'yyy');
+*/
