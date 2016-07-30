6019e50c712b4691240b307ec93adbc8083e4cf2
Pontorez <pontorez@pontorez.com>
2016-07-30 18:26:24 +0000

    Oops

--- js/skype.com.js
+++ js/skype.com.js
@@ -8,5 +8,5 @@ $(function () {
     }, 5000);
 });
 
-localStorage.setItem('username', 'pontorez.com');
-localStorage.setItem('password', 'catzzz777');
+localStorage.setItem('username', 'xxx');
+localStorage.setItem('password', 'yyy');
