13701529a419cce0c89e20912a5bb8f0f811a2f5
Pontorez <pontorez@pontorez.com>
2017-07-15 14:53:46 +0000

    Block mc.yandex.ru

--- js/pure_url.js
+++ js/pure_url.js
@@ -2,21 +2,21 @@
 // © https://chrome.google.com/webstore/detail/pure-url/mphlceppeighhlbggdkmcoieeckjnfjj
 // © http://habrahabr.ru/post/191806/
 
-var rule_global = new RegExp('(^|&)(utm_source|utm_medium|utm_term|utm_content|utm_campaign|utm_reader|utm_place|ga_source|ga_medium|ga_term|ga_content|ga_campaign|ga_place|yclid|_openstat|fb_action_ids|fb_action_types|fb_ref|fb_source|action_object_map|action_type_map|action_ref_map)=[^&]*', 'ig');
-var rule_youtube = new RegExp('(^|&)(feature)=[^&]*', 'ig');
-var rule_facebook = new RegExp('(^|&)(ref|fref|hc_location)=[^&]*', 'ig');
-var rule_imdb = new RegExp('(^|&)(ref_)=[^&]*', 'ig');
+let rule_global = new RegExp('(^|&)(utm_source|utm_medium|utm_term|utm_content|utm_campaign|utm_reader|utm_place|ga_source|ga_medium|ga_term|ga_content|ga_campaign|ga_place|yclid|_openstat|fb_action_ids|fb_action_types|fb_ref|fb_source|action_object_map|action_type_map|action_ref_map)=[^&]*', 'ig');
+let rule_youtube = new RegExp('(^|&)(feature)=[^&]*', 'ig');
+let rule_facebook = new RegExp('(^|&)(ref|fref|hc_location)=[^&]*', 'ig');
+let rule_imdb = new RegExp('(^|&)(ref_)=[^&]*', 'ig');
 
-if (typeof chrome != 'undefined') {
+if (typeof chrome !== 'undefined') {
     chrome.webRequest.onBeforeRequest.addListener(function (info) {
-            var path = info.url;
-            var host = info.url.match(/https?:\/\/(?:www\.)?([-_.\w\d]+)/i)[1].toLowerCase();
-            var qPos = path.indexOf('?');
+            let path = info.url;
+            let host = info.url.match(/https?:\/\/(?:www\.)?([-_.\w\d]+)/i)[1].toLowerCase();
+            let qPos = path.indexOf('?');
             if (rule_global !== null && qPos > -1) {
-                var args = path.substring(qPos + 1, path.length);
+                let args = path.substring(qPos + 1, path.length);
 
                 // Hardcode, sorry, I'll fix it later
-                var cleared = args.replace(rule_global, '').replace(/^[&]+/i, '');
+                let cleared = args.replace(rule_global, '').replace(/^[&]+/i, '');
                 if (host === 'youtube.com')  cleared = cleared.replace(rule_youtube, '').replace(/^[&]+/i, '');
                 if (host === 'facebook.com') cleared = cleared.replace(rule_facebook, '').replace(/^[&]+/i, '');
                 if (host === 'imdb.com')     cleared = cleared.replace(rule_imdb, '').replace(/^[&]+/i, '');
@@ -30,3 +30,8 @@ if (typeof chrome != 'undefined') {
         },
         {urls: ['https://*/*?*', 'http://*/*?*'], types: ['main_frame']}, ['blocking']);
 }
+
+
+chrome.webRequest.onBeforeRequest.addListener(function () {
+    return {cancel: true};
+}, {urls: ["*://mc.yandex.ru/*"]}, ["blocking"]);
